6a90265e878945727ed255558e209a832ab95307
devimicael <d7096439@gmail.com>
2023-08-05 14:01:06 -0300

    atividades 0.0.6

---
+++ atividades-plataforma/exer/04_array.js
@@ -0,0 +1,37 @@
+class Pessoa {
+    constructor(nome, idade){
+        this.nome = nome;
+        this.idade = idade;
+        this.proximo = null;
+    }
+}
+
+class ListPessoa {
+    constructor(){
+        this.cabeca = null;
+    }
+
+    add(nome, idade){
+        const novoPessoa = new Pessoa(nome, idade);
+        if(!this.cabeca){
+            this.cabeca = novoPessoa;
+        } else {
+            let atual = this.cabeca;
+            while(atual.proximo){
+                atual = atual.proximo;
+            }
+            atual.proximo = novoPessoa;
+        }
+    }
+    showPessoas(){
+        let atual = this.cabeca;
+        while(atual){
+            console.log(`Nome:${atual.nome} | Idade:${atual.idade}`);
+            atual = atual.proximo;
+        }
+    }
+}
+const l1 = new ListPessoa();
+l1.add("Davi", 24);
+l1.add("Ana", 28);
+l1.showPessoas();
--- atividades-plataforma/exer/extras.js
+++ atividades-plataforma/exer/extras.js
@@ -8,6 +8,8 @@ const desafio1 = [
     ["Lúcia", [7.0, 3.5, 9.0]],
     ["PEdro", [8.6, 7.3, 8.8]],
 ];
+const { link } = require("fs");
+const { type } = require("os");
 /* 
 for(let i in desafio1){
     let soma = 0;
@@ -18,3 +20,115 @@ for(let i in desafio1){
     console.log(`Nome:${desafio1[i][0]} | Média:${soma.toFixed(1)}`);
 }
 */
+
+
+class No{
+    constructor(nome){
+        this.nome = nome;
+        this.proximo = null;
+    }
+}
+
+class ListaLigada{
+    constructor(){
+        this.cabeca = null;
+    }
+
+    addFirst(nome){
+        const newNo = new No(nome);
+        newNo.proximo = this.cabeca;
+        this.cabeca = newNo;
+    }
+    addLast(nome){
+        const newNo = new No(nome);
+        if(!this.cabeca){
+            this.cabeca = newNo;
+        } else {
+            let current = this.cabeca;
+            while(current.proximo){
+                current = current.proximo;
+            }
+            current.proximo = newNo;
+        }
+    }
+    removeFirst(){
+        if(!this.cabeca){
+            return null;
+        }
+        const removeNo = this.cabeca;
+        this.cabeca = this.cabeca.proximo;
+        removeNo.proximo = null;
+        return removeNo.nome;
+    }
+    removeLast(){
+        if(!this.cabeca) return null;
+        if(!this.cabeca.proximo){
+            const removeNo = this.cabeca;
+            this.cabeca = null;
+            return removeNo.nome;
+        }
+        let current = this.cabeca;
+        let previous = null;
+        while(current.proximo){
+            previous = current;
+            current = current.proximo;
+        }
+        previous.proximo = null;
+        return current.nome;
+    }
+    serach(nome){
+        let current = this.cabeca;
+        while(current){
+            if(current.nome === nome) return current;
+            current = current.proximo;
+        }
+        return null;
+    }
+    size(){
+        let count = 0;
+        let current = this.cabeca;
+        while(current){
+            count += 1;
+            current = current.proximo;
+        }
+        return count;
+    }
+    printList(){
+
+    }
+}
+
+class Pessoa {
+    constructor(nome){
+        this.nome = nome;
+        this.proximo = null;
+    }
+}
+
+class FilaEspera{
+    constructor(){
+        this.primeiraPessoa = null;
+        this.ultimaPessoa = null;
+    }
+
+    entrarNaFila(nome){
+        const novaPessoa = new Pessoa(nome);
+        if(!this.primeiraPessoa){
+            this.primeiraPessoa = novaPessoa;
+            this.ultimaPessoa = novaPessoa;
+        } else {
+            this.ultimaPessoa.proximo = novaPessoa;
+            this.ultimaPessoa = novaPessoa;
+        }
+    }
+
+    atenderProxima(){
+        if(!this.primeiraPessoa){
+            return "A fila está vazia!";
+        }
+        const pessoaAtendida = this.primeiraPessoa;
+        this.primeiraPessoa = this.primeiraPessoa.proximo;
+        pessoaAtendida.proximo = null;
+        return `${pessoaAtendida.nome} foi atendida!`;
+    }
+}
