9868608f9ba14e86f8eac322a8d09a8c3935359b
devimicael <d7096439@gmail.com>
2023-08-05 09:10:31 -0300

    atividades 0.0.5

--- atividades-plataforma/exer/03_array.js
+++ atividades-plataforma/exer/03_array.js
@@ -1,12 +1,31 @@
-const array = [3,7,9,1,0];
-console.log(array);
+let array = [3, 7 ,9, 1,0];
+const sizeArray = array.length
 
 const filaRemove = () => array.shift();
 const listaRemove = (el) => array.splice(array.indexOf(el), 1); 
 const pilhaRemove = () => array.pop();
 
+/* 
 console.log("fila:", filaRemove());
 console.log("lista:", listaRemove(7));
 console.log("pilha:", pilhaRemove());
 
-console.log(array);
+*/
+// * filas
+for(let i=0; i < sizeArray; i++){
+    filaRemove();
+    console.log(i, array);
+}
+
+// * listas
+array = [3, 7 ,9, 1,0];
+for(let i=0; i < sizeArray; i++){
+    listaRemove(i)
+    console.log(i, array);
+}
+// * pilhas
+array = [3, 7 ,9, 1,0];
+for(let i=0; i < sizeArray; i++){
+    pilhaRemove()
+    console.log(i, array);
+}
